1b969ee455a6fab13e837953cd1d2abfe6df8417
Ali Mohamed <110411510+alim-99@users.noreply.github.com>
2024-09-25 20:33:11 +0300

    Add files via upload

---
+++ src/app/app/profile/page.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = profilePage;
+function profilePage() {
+    return (<div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Profile</h1>
+      <hr />
+      <p>Profile Page</p>
+    </div>);
+}
