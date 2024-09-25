c0833a5986d868698b58427e55f32e4295c88f0a
Ali Mohamed <110411510+alim-99@users.noreply.github.com>
2024-09-25 20:31:36 +0300

    Add files via upload

---
+++ src/app/models/userModel.js
@@ -0,0 +1,35 @@
+import mongoose, { mongo } from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Please provide a username"],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide a email"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    unique: true,
+  },
+  isVerfied: {
+    type: Boolean,
+    default: false,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  forgotPasswordToken: String,
+  forgotPasswordTokenExpiry: Date,
+  verfiyToken: String,
+  verfiyTokenExpiry: Date,
+})
+
+const User = mongoose.models.users || mongoose.model("users", userSchema)
+
+export default User;
